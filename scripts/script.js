b7686301aac31ce6404480c82e39fe2da5b6f8ac
MassayoshiDev <psnnapoleandro@gmail.com>
2025-04-01 09:19:30 -0300

    add - pronto para os amigos iniciarem a parte deles

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,4 @@
 function print(msg) {
-    console.log(msg)
+    console.log(msg);
 }
 
-/*
-
-document.getElementById("minhaImagem").addEventListener("click", function() {
-    alert("Você clicou na imagem!");
-});
-
-*/
