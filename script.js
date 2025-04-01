12742b398393df7516a3b170eb93606d760d0270
MassayoshiDev <psnnapoleandro@gmail.com>
2025-04-01 08:53:20 -0300

    start

---
+++ script.js
@@ -0,0 +1,11 @@
+function print(msg) {
+    console.log(msg)
+}
+
+/*
+
+document.getElementById("minhaImagem").addEventListener("click", function() {
+    alert("Você clicou na imagem!");
+});
+
+*/
