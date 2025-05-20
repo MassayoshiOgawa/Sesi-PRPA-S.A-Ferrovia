c16c807e2117e9194cb948198f25e2f99b43f327
MassayoshiDev <psnnapoleandro@gmail.com>
2025-05-20 12:57:55 -0300

    criado uma pequena parte do código de notificaçoes

--- scripts/OpcoesBarraLateral/notificacoes.js
+++ scripts/OpcoesBarraLateral/notificacoes.js
@@ -2,12 +2,43 @@ function relatar() {
     window.location.replace("../public/escreverNotificacao.html")
 }
 
+function voltar(n) {
+    if (n == 1) {
+        window.location.replace("../public/notificacoes.html")
+    } else {
+        window.location.replace("../public/homepage.html")
+    }
+}
+
+function enviarFormulario() {
+    for (var i = 0; i < 99; i++) {
+        if (!localStorage.getItem(`notiTrem${i}`)) {
+            localStorage.setItem(`notiTrem${i}`, i)
+            const form = document.getElementById("escreverNotificacao_form")
+            localStorage.setItem(`notiTrem${i}_nome`, form.inNome.value)
+            localStorage.setItem(`notiTrem${i}_desc`, form.inDesc.value)
+            localStorage.setItem(`notiTrem${i}_grau`, form.inGrau.value)
+            localStorage.setItem(`notiTrem${i}_horario`, form.inHorario.value)
+            alert("registrado")
+            break
+        } 
+    }
+}
+
 const aux = document.getElementById("mainNoticia");
 const container = document.getElementById("containerNotificacoes");
 const noticiaOriginal = aux.cloneNode(true);
-aux.remove();
 
-for (let i = 0; i < 2; i++) {
-    const clone = noticiaOriginal.cloneNode(true);
-    container.appendChild(clone);
+for (var i = 0; i < 99; i++) {
+    if (localStorage.getItem(`notiTrem${i}`)) {
+        const clone = noticiaOriginal.cloneNode(true);
+        clone.getElementById("noti_nome").textContent = localStorage.getItem(`notiTrem${i}_nome`)
+        clone.getElementById("noti_desc").textContent = localStorage.getItem(`notiTrem${i}_desc`)
+        clone.getElementById("noti_estado").textContent = localStorage.getItem(`notiTrem${i}_estado`)
+        clone.getElementById("noti_grau").textContent = localStorage.getItem(`notiTrem${i}_grau`)
+        clone.getElementById("noti_horario").textContent = localStorage.getItem(`notiTrem${i}_horario`)
+        container.appendChild(clone);
+    } else {
+        break
+    }
 }
