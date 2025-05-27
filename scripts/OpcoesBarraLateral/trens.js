800807918adcc3c632c60ef9ba5ba0c8b1b1f662
Pedro <pedro_h_bueno@estudante.sesisenai.org.br>
2025-05-27 09:33:38 -0300

    inserção do triangulo na página trens e sua lógica

--- scripts/OpcoesBarraLateral/trens.js
+++ scripts/OpcoesBarraLateral/trens.js
@@ -2,34 +2,47 @@ function print(msg) {
     console.log(msg)
 }
 
-let bobo = false;
+let estado = false;
 let tamanho = "0" ;
 let currBox = null;
+let currTri = null
 
 function abrir(box){
+    var tri = box.querySelector("img");
+
     if (tamanho !== "0" || currBox !== null){
         if (currBox !== null && currBox !== box) { 
             tamanho = "50px";
             currBox.style.height = tamanho;
+            currTri.style.transform = "rotate(0deg)";
             print("A")
             tamanho = "1000px";
             box.style.height = tamanho;
-            bobo = false;
+            tri.style.transform = "rotate(180deg)";
+            estado = false;
             currBox = box;
+            currTri = tri;
         }
     }
-
-    if (bobo == false){
+    
+    if (estado == false){
         tamanho = "1000px";
+        tri.style.transform = "rotate(180deg)";
         box.style.height = tamanho;
-        bobo = true;
+        estado = true;
         currBox = box;
+        currTri = tri;
         print("B")
     }else{
+        tri.style.transform = "rotate(0deg)";
         tamanho = "50px";
         box.style.height = tamanho;
-        bobo = false;
+        estado = false;
         currBox = null;
+        currTri = null;
         print("C")
     }
+
+    
+
 }
