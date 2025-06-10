c5057a5bd1e342bd0f37c115380916fcba991fcf
pdrlbueno <pedro_h_bueno@estudante.sesisenai.org.br>
2025-06-10 09:34:57 -0300

    validação de login por JavaScript

--- scripts/login.js
+++ scripts/login.js
@@ -3,37 +3,31 @@ function print(msg) {
 }
 
 
-/* Dois vetores que armazenam os usuarios e as senhas de forma respectiva */
-const usuarios = ["adm", "Leandro Massayoshi Ogawa", "Pedro Henrique Monteiro Oliveira", "Enzo Souza"]
-const senhas = ["adm", "abacaxi123", "javascript123", "porco321"]
-
-
 function enviarform() {
-    const frm = document.getElementById("loginForm")
-    const nome = frm.inNome.value.trim();
-    const senha = frm.inSenha.value.trim();
-    const cadastro = parseInt(frm.inCadastro.value)
+    const form = document.getElementById("loginForm");
+    const nome = form.inNome.value.trim();
+    const senha = form.inSenha.value.trim();
+    const cadastro = form.inCadastro.value;
 
-    if (nome.toLowerCase() !== "adm") {
-        if (nome !== usuarios[cadastro]) {
-            alert("Usuário não encontrado.");
-            return;
-        } else if (senha !== senhas[cadastro]) {
-            alert("Senha incorreta.");
-            return;
-        } else {
-            alert("Login efetuado com sucesso!")
-            if (!localStorage.getItem("user")) {
-                localStorage.setItem("user", nome)
-            }
-            window.location.replace("../public/homepage.html")
-        }
-    } else {
-        alert("Login efetuado com sucesso!")
-        if (!localStorage.getItem("user")) {
-                localStorage.setItem("user", nome)
-            }
-        window.location.replace("../public/homepage.html")
+
+    if (nome == "") {
+        alert("Nome é obrigatório");
+        return;
+    } else if (senha == "") {
+        alert("Senha é obrigatório");
+        return;
+    } else if (cadastro == "") {
+        alert("Cadastro é obrigatório");
+        return;
     }
+
+    if (senha.length !== 8) {
+        alert("Senha precisa de 8 caracteres")
+        return;
+    }
+
+    console.log(nome);
+    console.log(senha);
+    console.log(cadastro);
 }
 
