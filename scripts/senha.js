0bb1e1f0c1dbf781ae274f56584195d05de6ad29
Pedro <pedro_h_bueno@estudante.sesisenai.org.br>
2025-06-12 20:18:20 -0300

    terminado a página "senha.html", para recuperar senha

---
+++ scripts/senha.js
@@ -0,0 +1,20 @@
+function enviarEmail() {
+    const form = document.getElementById("SenhaForm");
+    const email = form.REmail.value.trim();
+
+
+
+    if (email == "") {
+        alert("Campo obrigatório");
+        return;
+    }
+
+  
+    alert("Verefique seu email para mudar sua senha")
+
+}
+
+function irLogin() {
+    window.location.replace("../public/index.html")
+
+}
