37c4a3986df71ec054dd62beb117a3d13182c7ee
pdrlbueno <pedro_h_bueno@estudante.sesisenai.org.br>
2025-11-28 02:23:24 -0300

    style perfil

--- public/perfil.php
+++ public/perfil.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <script src="../scripts/OpcoesBarraLateral/perfil.js"></script>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,33 +14,64 @@
     <header style="background-color: rgba(250, 235, 215, 0);">
         <img class="voltarICON" src="../assets/setaICON.png" alt="" onclick="voltar()">
     </header>
-    <?php 
-        include '../db.php';
-        session_start();
-        $idUser =  $_SESSION['id_usuario'];
-
-        $sql = "SELECT * FROM usuario WHERE id_usuario =  $idUser";
-
-        $result = $mysqli->query($sql);
-        $row = $result->fetch_assoc();
-
-        echo"
-        <img class='PerfilIcon' src='../assets/userICON.png' alt=''>
-        <div  class='relatarContainer'>
+    <main>
         
-            <p class='relatarInput'> Email: {$row['email_usuario']}</p><br>
-            <p class='relatarInput'> Senha: {$row['senha_usuario']}</p><br>
-            <p class='relatarInput'> Telefone: {$row['telefone_usuario']}</p><br>
-            <p class='relatarInput'> Cargo: {$row['cargo_usuario']}</p><br>
-            <p class='relatarInput'> Data de Nascimento: {$row['nascimento_usuario']}</p><br>
-            <p class='relatarInput'> Nome: {$row['nome_usuario']}</p><br>
-            <br>
-            <button type= 'submit' class='botaoEnviarPerfil' > Enviar</button>
-    
-        </div>
-        ";
+            <?php
+            include '../db.php';
+            session_start();
+            $idUser =  $_SESSION['id_usuario'];
 
-    ?>
+            $sql = "SELECT * FROM usuario WHERE id_usuario =  $idUser";
+
+            $result = $mysqli->query($sql);
+            $row = $result->fetch_assoc();
+
+            echo "
+            <img class='PerfilIcon' src='../assets/userICON.png' alt=''>
+            <div id='form'>
+                <div  class='formCadastro'>
+        
+                    <div class='inpput'>
+                        <p class='relatarInput'> Email: {$row['email_usuario']}</p>
+                    </div>
+                    <br>
+        
+                    <div class='inpput'>
+                        <p class='relatarInput'> Senha: {$row['senha_usuario']}</p>
+                    </div>
+                    <br>
+        
+                    <div class='inpput'>
+                        <p class='relatarInput'> Telefone: {$row['telefone_usuario']}</p>
+                    </div>
+                    <br>
+        
+                    <div class='inpput'>
+                        <p class='relatarInput'> Cargo: {$row['cargo_usuario']}</p>
+                    </div>
+                    <br>
+        
+                    <div class='inpput'>
+                        <p class='relatarInput'> Data de Nascimento: {$row['nascimento_usuario']}</p>
+                    </div>
+                    <br>
+                    
+                    <div class='inpput'>
+                        <p class='relatarInput'> Nome: {$row['nome_usuario']}</p>
+                    </div>
+                    <br>
+                    <br>
+                    
+            
+                </div>
+            </div>
+            ";
+            ?>
+            <div class="relatarButton">
+            <button type="submit" class="botaoRelatar"> Atualizar</button>
+            </div>
+        </div>
+    </main>
 </body>
 
 </html>
